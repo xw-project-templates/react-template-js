0f80d13e373153128e42b54ae9ff7c573b741a94
cxw <1660509931@qq.com>
2023-08-16 17:39:09 +0800

    feat(): node-ssh版本升级

--- deploy/config.js
+++ deploy/config.js
@@ -1,11 +1,7 @@
 
 module.exports = options = {
     host: "",
-<<<<<<< HEAD
-    port: "22",
-=======
     port: "",
->>>>>>> 4a049b4a3874c77e9dd61f17db4d32de56af2ec8
     username: "",
     password: "",
     removePath: ""
